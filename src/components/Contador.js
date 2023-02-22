231ce3a378df86bf06248c4bdefd8d3f285517d6
DYI-murri <110579758+DYI-murri@users.noreply.github.com>
2023-02-22 14:26:48 -0600

    Update Contador.js

--- src/components/Contador.js
+++ src/components/Contador.js
@@ -7,7 +7,7 @@ const Contador = () => {
   return (
     <div>
       <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 3)}>
+      <button onClick={() => setCount(count + 1)}>
         Click me
       </button>
     </div>
